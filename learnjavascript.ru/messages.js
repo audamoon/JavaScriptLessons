20c3300128fcc2166820fe26696edbdfcacd63d6
audamoon <audamoon@gmail.com>
2022-10-05 22:16:56 +0300

    Messages in JS
    To start learning Node without JS base knowleage was a mistake :angry:
    So I deleted all of npm stuff and back to JS base.
    So today I learned messages and how to implement JS code into html page :+1:
    Unfortunately, I'll work next week, so maybe I will spend less time studying :cry: :cry: :cry:

---
+++ learnjavascript.ru/messages.js
@@ -0,0 +1,27 @@
+function messagesDemo() {
+
+    let userAnswer = confirm('Do you want to enter your name?');
+
+    if (userAnswer == true) {
+        let userName =prompt('What is your name?', 'enter your name here');
+
+        if (userName != null) {
+            alert(`Hello, ${userName}! :D`);
+        } else {
+            alert(`Error, try again >:c`);
+        }
+
+    } else {
+        alert('Okay, sorry :c')
+    }
+    
+    
+    
+};
+/*
+Или можно использовать 
+document.getElementById('elementId').function name(params) {
+    
+}
+и добавить id к тегу <button>
+*/
--- myFirstJSProgram.js
+++
@@ -1,8 +0,0 @@
-const welcome = person => {
-    const name = person.name;
-    person.height = 123;
-    height = person.height;
-    console.log('Hi ${name}, youre ${height}');
-}
-
-welcome ({name :"Patrik", height: 123})
